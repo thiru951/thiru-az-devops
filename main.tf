b2814d041d2189097a362651aa6a89d58d455ce5
thiru951 <unvantedsw@gmail.com>
2026-04-21 05:23:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "thiru-rg"
-  location = "South India"
+  location = "southindia"
 }
 
 resource "azurerm_virtual_network" "vnet" {
