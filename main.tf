b3ced7420aedaccb055a210b13c779a02bb997f2
thiru951 <unvantedsw@gmail.com>
2026-04-21 15:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,28 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "thiru-rg"
-  location = "centralindia"
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "thiru-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "centralindia"
+  resource_group_name = "thiru-rg"
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "thiru-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = "thiru-rg"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
   name                = "thiru-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "centralindia"
+  resource_group_name = "thiru-rg"
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "thiru-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "centralindia"
+  resource_group_name = "thiru-rg"
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -39,8 +34,8 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "thiru-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = "centralindia"
+  resource_group_name = "thiru-rg"
 
   security_rule {
     name                       = "RDP"
@@ -62,8 +57,8 @@ resource "azurerm_network_interface_security_group_association" "nsgassoc" {
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "thiru-winvm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = "thiru-rg"
+  location            = "centralindia"
   size                = "Standard_B2s"
 
   admin_username = "azureuser"
