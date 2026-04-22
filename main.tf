a3359fe9dbaf7b86f9c1b2f75444f9d5517f44ab
thiru951 <unvantedsw@gmail.com>
2026-04-22 15:32:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,76 +1,85 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "thiru-rg"
+  location = "Central India"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "thiru-vnet"
-  location            = "eastus"
-  resource_group_name = "thiru-rg"
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "thiru-subnet"
-  resource_group_name  = "thiru-rg"
+  name                 = "thiru-sub"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "thiru-pip"
-  location            = "eastus"
-  resource_group_name = "thiru-rg"
-
-  allocation_method = "Static"
-  sku               = "Standard"
-}
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "thiru-nsg"
-  location            = "eastus"
-  resource_group_name = "thiru-rg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "RDP"
-    priority                   = 100
+    name                       = "Allow-RDP"
+    priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range         = "*"
-    destination_port_range    = "3389"
-    source_address_prefix     = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
 
+resource "azurerm_public_ip" "pip" {
+  name                = "thiru-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "thiru-nic"
-  location            = "eastus"
-  resource_group_name = "thiru-rg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig1"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id         = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "nsgassoc" {
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "thiru-winvm"
-  resource_group_name = "thiru-rg"
-  location            = "eastus"
-  size                = "Standard_B1s"
+  name                = "thiru-win19-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
 
-  admin_username = "azureuser"
-  admin_password = "Password@12345!"
+  admin_username = var.admin_username
+  admin_password = var.admin_password
 
   network_interface_ids = [
-    azurerm_network_interface.nic.id
+    azurerm_network_interface.nic.id,
   ]
 
   os_disk {
+    name                 = "thiru-osdisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
